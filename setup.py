526cf51b57f5e4d1ed9d545fc274e2f6510d15e1
Matt Odden <mrodden@us.ibm.com>
2018-12-04 12:01:44 -0600

    Rename readme back to markdown form

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+from os import path
+curr_dir = path.abspath(path.dirname(__file__))
+with open(path.join(curr_dir, 'README.md')) as readme:
+    readme_content = readme.read()
+
 setup(
     name="iam-token-manager",
     version="0.1.0",
@@ -7,4 +12,7 @@ setup(
     py_modules=["bxauth"],
     maintainer='Mathew Odden',
     maintainer_email='mathewrodden@gmail.com',
+
+    long_description=readme_content,
+    long_description_content_type='text/markdown',
 )
