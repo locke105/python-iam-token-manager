16940e951dd6ccb2aa4f1cfccacc9b50b4ad39e2
Matt Odden <mrodden@us.ibm.com>
2018-12-04 11:48:07 -0600

    Add python setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="iam-token-manager",
+    version="0.1.0",
+    url="https://github.com/locke105/python-iam-token-manager",
+    py_modules=["bxauth"],
+    maintainer='Mathew Odden',
+    maintainer_email='mathewrodden@gmail.com',
+)
